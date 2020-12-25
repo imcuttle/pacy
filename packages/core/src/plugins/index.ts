70a659d9836362102687e615a6442a09c84b92e9
余聪 <yucong06@meituan.com>
2020-12-25 19:21:15 +0800

    chore: wip

--- packages/core/src/plugins/index.ts
+++ packages/core/src/plugins/index.ts
@@ -1,8 +1,10 @@
-import { readdirSync } from 'fs'
+import { readdirSync, statSync } from 'fs'
 import { resolve } from 'path'
 
 import { TPacyPlugin } from '..'
-const pluginNames = readdirSync(__dirname).map((name) => resolve(__dirname, name))
+const pluginNames = readdirSync(__dirname)
+  .map((name) => resolve(__dirname, name))
+  .filter((filename) => statSync(filename).isDirectory())
 
 export default function presetDefault() {
   return function (api) {
--- packages/routes-webpack-plugin/__tests__/fixture/_routesPlaceholder.js
+++ packages/routes-webpack-plugin/__tests__/fixture/_routesPlaceholder.js
@@ -1,26 +1,6 @@
 module.exports = [
-  {
-    name: './pages/a.js',
-    source: function () {
-      return import('/Users/yucong06/code/pacy/packages/routes-webpack-plugin/__tests__/fixture/pages/a.js')
-    }
-  },
-  {
-    name: './pages/b.js',
-    source: function () {
-      return import('/Users/yucong06/code/pacy/packages/routes-webpack-plugin/__tests__/fixture/pages/b.js')
-    }
-  },
-  {
-    name: './pages/c.js',
-    source: function () {
-      return import('/Users/yucong06/code/pacy/packages/routes-webpack-plugin/__tests__/fixture/pages/c.js')
-    }
-  },
-  {
-    name: './pages/d.js',
-    source: function () {
-      return import('/Users/yucong06/code/pacy/packages/routes-webpack-plugin/__tests__/fixture/pages/d.js')
-    }
-  }
+  '/Users/yucong06/code/pacy/packages/routes-webpack-plugin/__tests__/fixture/pages/a.js',
+  '/Users/yucong06/code/pacy/packages/routes-webpack-plugin/__tests__/fixture/pages/b.js',
+  '/Users/yucong06/code/pacy/packages/routes-webpack-plugin/__tests__/fixture/pages/c.js',
+  '/Users/yucong06/code/pacy/packages/routes-webpack-plugin/__tests__/fixture/pages/d.js'
 ]
